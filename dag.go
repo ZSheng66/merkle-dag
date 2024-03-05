c2372448d6cf412b654552887f8872e5ee0a6418
ZSheng66 <2672422083@qq.com>
2024-03-05 20:10:11 +0800

    the first lab.

--- dag.go
+++ dag.go
@@ -3,6 +3,29 @@ package merkledag
 import "hash"
 
 func Add(store KVStore, node Node, h hash.Hash) []byte {
-	// TODO 将分片写入到KVStore中，并返回Merkle Root
-	return nil
+    // 将对象序列化为字节数组
+    data, err := Encode(node)
+    if err != nil {
+        log.Errorf("Failed to encode node: %v", err)
+        return nil
+    }
+
+    // 对字节数组进行哈希计算并记录哈希值
+    h.Reset()
+    _, err = h.Write(data)
+    if err != nil {
+        log.Errorf("Failed to hash data: %v", err)
+        return nil
+    }
+    hashVal := h.Sum(nil)
+    
+    // 将哈希值和字节数组写入到 KVStore 中
+    err = store.Put(hashVal, data)
+    if err != nil {
+        log.Errorf("Failed to put data into KVStore: %v", err)
+        return nil
+    }
+
+    // 返回哈希链根节点
+    return hashVal
 }
--- kvstore.go
+++ kvstore.go
@@ -1,5 +1,9 @@
 package merkledag
-
+/* Has(key []byte) (bool, error)：用于检查指定的键是否存在于存储中，返回布尔值表示是否存在以及可能的错误。
+Put(key, value []byte) error：用于向存储中添加或更新指定的键值对，如果操作成功则返回nil，否则返回错误。
+Get(key []byte) ([]byte, error)：用于获取指定键对应的值，同时返回值和可能的错误。
+Delete(key []byte) error：用于删除存储中指定的键值对，成功则返回nil，否则返回错误
+ */
 type KVStore interface {
 	Has(key []byte) (bool, error)
 	Put(key, value []byte) error
